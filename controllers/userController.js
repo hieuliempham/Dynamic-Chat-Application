e9b0caa41fe7e1c312fb3077a41cf10b9b1a0f2f
Karry_Pham <85409706+hieuliempham@users.noreply.github.com>
2024-04-09 20:58:01 +0700

    [Add] User Login and Log Out with Session

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,7 +33,57 @@ const register = async(req, res)=>{
     }
 }
 
+const loadLogin = async(req, res)=>{
+    try {
+        res.render('login');
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const login = async(req, res)=>{
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const userData = await User.findOne({email: email});
+        if (userData) {
+                const passwordMatch = bcrypt.compare(password, userData.password);
+                if(passwordMatch){
+                    req.session.user = userData;
+                    res.redirect('/dashboard');
+                }else{
+                    res.render('login', { message: "Email hoặc Password không chính xác"});
+                }
+        }else{
+            res.render('login', { message: "Email hoặc Password không chính xác"});
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+const logout = async(req, res)=>{
+    try {
+        req.session.destroy();
+        res.redirect('/');
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+const loadDashboard = async(req, res)=>{
+    try {
+        res.render('dashboard', {user: req.session.user})
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
 module.exports = {
     registerLoad,
-    register
+    register,
+    loadLogin,
+    login,
+    logout,
+    loadDashboard
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,9 +3,11 @@ const path = require('path');
 const multer = require('multer');
 const bodyParser = require('body-parser');
 const userController = require('../controllers/userController');
-
+const session = require('express-session');
 
 const user_route = express();
+const {SESSION_SECRET} = process.env;
+user_route.use(session({secret: SESSION_SECRET}));
 
 user_route.set('view engine', 'ejs');
 user_route.set('views', path.join(__dirname, '..', 'views'));
@@ -28,8 +30,20 @@ const storage = multer.diskStorage({
 const upload = multer({storage:storage});
 
 
-
+// Dang ky
 user_route.get('/register', userController.registerLoad);
 user_route.post('/register',upload.single('image'), userController.register);
+
+// Dang nhap
+user_route.get('/', userController.loadLogin);
+user_route.post('/', userController.login);
+user_route.get('/logout', userController.logout);
+
+// dashboard
+user_route.get('/dashboard', userController.loadDashboard);
+
+user_route.get('*', function(req, res){
+    res.redirect("/");
+})
 
 module.exports = user_route;
